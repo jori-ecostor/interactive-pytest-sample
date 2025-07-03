5009e3b8e532a9b7c74fdf4d99c2a9edb2e352ea
Jonathan Rico <jonathan.rico@eco-stor.no>
2025-07-03 11:12:10 +0200

    Add basic input

--- conftest.py
+++ conftest.py
@@ -8,11 +8,34 @@ logger = logging.getLogger(__name__)
 
 g_response = False
 
-def web_ui_handler(request: Request):
+inputpage = """
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User Input</title>
+</head>
+<body>
+    <form action="/userinput" method="POST">
+        <input type="text" name="usertext" required>
+        <button type="submit">Submit</button>
+    </form>
+</body>
+</html>"""
+
+def web_output_handler(request: Request):
+    return Response(inputpage, content_type="text/html")
+
+def web_input_handler(request: Request):
     global g_response
     g_response = True
 
-    return Response("Heyo")
+    resp = request.form['usertext']
+
+    rsp = f'Your answer: {resp}'
+
+    return Response(rsp)
 
 def wait_for_input():
     # TODO: add timeout
@@ -23,5 +46,6 @@ def wait_for_input():
 
 @pytest.fixture
 def user_input(httpserver):
-    httpserver.expect_request("/input").respond_with_handler(web_ui_handler)
+    httpserver.expect_request("/input").respond_with_handler(web_output_handler)
+    httpserver.expect_request("/userinput").respond_with_handler(web_input_handler)
     # TODO: return class with .wait and .get objects
